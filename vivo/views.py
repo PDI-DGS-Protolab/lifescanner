5dde8e89adcc6526fb217fd747d59d92031c0ccc
unknown <olga.dziegielewska@BRUJAS.tolkien.fib.upc.es>
2013-10-21 13:45:19 +0200

    Client-Server connection[not finished, gives 303 response]: ----dataScanner2.js: should capture the HttpResponse

--- vivo/views.py
+++ vivo/views.py
@@ -3,14 +3,24 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.conf import settings
+from django.template import Context, loader
+from django.shortcuts import render_to_response
 
-import urllib
+import urllib, json
 
-@login_required
+#@login_required
 def index(request):
+    #tmp=programs(request)
+    #print tmp
+    #return render(request, 'index.html', tmp)
     return render(request, 'index.html', {})
 
-def programs(request):
-    json = urllib.urlopen(settings.ENDPOINT)
 
-    return HttpResponse(json, mimetype="application/json", status=200)
+def programs(request):
+    json1 = urllib.urlopen(settings.ENDPOINT)
+    #json_dumped = json.dumps(json.loads(json1.read()))
+
+    #t=loader.get_template('index.html')
+    #c = Context(json1)
+    #return render_to_response('index.html', json1, content_type="application/json")
+    return HttpResponse(json1, mimetype="application/json", status=200)
