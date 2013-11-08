b3f444de9b33536cd2710fa152a68a7493404137
developer <boschpons@gmail.com>
2013-11-08 12:30:25 +0100

    Solved problems with GET function, all tests working equal

--- tests/static/js/genericListCtrl.test.js
+++ tests/static/js/genericListCtrl.test.js
@@ -5,18 +5,11 @@ describe('GenericListCtrl', function () {
         module('liveScannerApp');
     });
 
-
-    describe(' when attempting fake HTTP connections for getting the source API content', function() {
-        
-        var $httpBackend, $rootScope, createController,$controller;
+    var $httpBackend, $rootScope, createController,$controller;
 
         beforeEach(inject(function($injector) {
             $httpBackend = $injector.get('$httpBackend');
-
-            //TODO : check if it can be replaced with getting 
-            //the JSON data directly from the file
             $httpBackend.whenGET("/programs").respond(getData());
-
             $rootScope = $injector.get('$rootScope');   
             $controller = $injector.get('$controller');
 
@@ -25,165 +18,161 @@ describe('GenericListCtrl', function () {
                 '$scope' : $rootScope 
             });
             };
-
-        }));
-
-        it("should perform GET http call to /programs", inject(function ($httpBackend) {
-            $httpBackend.expectGET('/programs');
-            var controller = createController();
-            $httpBackend.flush();
-            expect($rootScope.data.showsPropertiesOfInterest[0].title).not.toEqual("");
-            //console.log($rootScope.data.showsPropertiesOfInterest);
         }));
 
 
-        it('SelectCtrl: correct deleting from unselected list', function () {
-
-            $httpBackend.expectGET('/programs');
-            var controller = createController();
-            $httpBackend.flush();
-            var scope=$rootScope;
-
-            //scope.data.showsPropertiesOfInterest = new ShowsArray(['a', 'b']);
-            expect(scope.data.showsPropertiesOfInterest.length).toBe(3);
-            scope.select(scope.data.showsPropertiesOfInterest[0]);
-            scope.$digest();
-
-            console.log(scope.data.showsPropertiesOfInterest.length);
-            console.log(scope.data.markedShows);
-            expect(scope.data.showsPropertiesOfInterest.length).toBe(2);
-            expect(scope.data.showsPropertiesOfInterest[0].title).toEqual("LegiÃ£o Estrangeira");
-            scope.select(scope.data.showsPropertiesOfInterest[1]);
-            scope.$digest();
-            expect(scope.data.showsPropertiesOfInterest.length).toBe(1);
-        });
-
-
-        //TODO : add get reference as in the function above
-        xit('SelectCtrl: correct move from unselected list to selected list', function () {
-            // $httpBackend.expectGET('/programs');
-            // var controller = createController();
-            // $httpBackend.flush();
-            // var scope=$rootScope;
-
-            scope.data.markedShows = new ShowsArray(['d']);
-            scope.data.showsPropertiesOfInterest = new ShowsArray(['a','b','c']);
-            var temp=scope.data.showsPropertiesOfInterest[0];
-            scope.select(scope.data.showsPropertiesOfInterest[0]);
-
-            scope.$digest();
-            expect(scope.data.showsPropertiesOfInterest.length).toBe(2);
-
-            expect(scope.data.markedShows.length).toBe(2);
-
-            expect(scope.data.markedShows[1]).toBe(temp);
-        });
-
-        //TODO : add get reference...
-        xit('SelectCtrl: checkboxState equals to false', function () {
-            scope.data.showsPropertiesOfInterest = new ShowsArray([{"id":0,ceckboxState:true},{"id":1,ceckboxState:true}]);
-            scope.select(scope.data.showsPropertiesOfInterest[0]);
-            scope.$digest();
-            expect(scope.data.markedShows[0].checkboxState).toBe(false);
-        });
-        
+   describe(' when attempting fake HTTP connections for getting the source API content', function() {
+      it("should perform GET http call to /programs", inject(function ($httpBackend) {
+        $httpBackend.expectGET('/programs');
+        var controller = createController();
+        $httpBackend.flush();
+        expect($rootScope.data.showsPropertiesOfInterest[0].title).not.toEqual("");
+      }));
     });
 
+    describe('SelectCtrl', function() {
+        
+      it('correct deleting from unselected list', function () {
+        httpBackend.expectGET('/programs');
+        var controller = createController();
+        $httpBackend.flush();
+        var scope=$rootScope;
+        expect(scope.data.showsPropertiesOfInterest.length).toBe(3);
+        scope.select(scope.data.showsPropertiesOfInterest[0]);
+        scope.$digest();
+        expect(scope.data.showsPropertiesOfInterest.length).toBe(2);
+        expect(scope.data.showsPropertiesOfInterest[0].title).toEqual("LegiÃ£o Estrangeira");
+        scope.select(scope.data.showsPropertiesOfInterest[1]);
+        scope.$digest();
+        expect(scope.data.showsPropertiesOfInterest.length).toBe(1);
+      });
 
-//TODO : join with GET request
-xdescribe('unSelectCtrl', function () {
-        var arr;
-        beforeEach(inject(function ($rootScope, $controller) {
-            scope = $rootScope.$new();
-            controller = $controller('GenericListCtrl', {
-                '$scope': scope
-            });
-            arr = new ShowsArray([
-                {id: 1234, title: 'element1', channel: {logo: null}, description: ""}, 
-                {id: 2640, title: 'element2', channel: {logo: null}, description: ""},
-                {id: 3000, title: 'ELEMENT3', channel: {logo: null}, description: ""}
-            ]);
-        }));
+      it('correct move from unselected list to selected list', function () {
+        $httpBackend.expectGET('/programs');
+        var controller = createController();
+        $httpBackend.flush();
+        var scope=$rootScope;
+        
+        var temp=scope.data.showsPropertiesOfInterest[0];
+        scope.select(scope.data.showsPropertiesOfInterest[0]);
 
-        it('Correct deleting from selected list', function () {
-            scope.data.markedShows = arr;
-            scope.unSelect(scope.data.markedShows[0]);
-            scope.$digest();
-            expect(scope.data.markedShows.length).toBe(2);
-            expect(scope.data.markedShows[0].title).toBe("element2");
+        scope.$digest();
+        expect(scope.data.showsPropertiesOfInterest.length).toBe(2);
 
-            scope.unSelect(scope.data.markedShows[0]);
-            scope.$digest();
-            expect(scope.data.markedShows.length).toBe(1);
+        expect(scope.data.markedShows.length).toBe(1);
 
-            scope.unSelect(scope.data.markedShows[0]);
-            scope.$digest();
-            expect(scope.data.markedShows.length).toBe(0);
+        expect(scope.data.markedShows[0]).toBe(temp);
+      });
 
-            
-        });
-
-        it('Correct move from selected list to unselected list', function () {
-            scope.data.markedShows = arr;
-            var temp=scope.data.markedShows[2];
-
-            scope.unSelect(scope.data.markedShows[2]);
-
-            scope.$digest();
-            expect(scope.data.markedShows.length).toBe(2);
-
-            expect(scope.data.showsPropertiesOfInterest.length).toBe(1);
-
-            expect(scope.data.showsPropertiesOfInterest[0]).toBe(temp);
-        });
-
-        it('Correct ordered insert in unselected list', function () {
-            scope.data.showsPropertiesOfInterest = arr;
-            scope.data.markedShows = new ShowsArray([
-                {id: 1234, title: 'element0', channel: {logo: null}, description: ""},
-                {id: 1234, title: 'element4', channel: {logo: null}, description: ""}]);
-            scope.unSelect(scope.data.markedShows[0]);
-            scope.$digest();
-            expect(scope.data.showsPropertiesOfInterest[0].title).toBe("element0");
-
-            scope.unSelect(scope.data.markedShows[0]);
-            scope.$digest();
-            expect(scope.data.showsPropertiesOfInterest[4].title).toBe("element4");
-        });
-
+      it('checkboxState equals to false', function () {
+        $httpBackend.expectGET('/programs');
+        var controller = createController();
+        $httpBackend.flush();
+        var scope=$rootScope;
+        scope.select(scope.data.showsPropertiesOfInterest[0]);
+        scope.$digest();
+        expect(scope.data.markedShows[0].checkboxState).toBe(false);
+      });        
     });
 
-xdescribe('toggleCheckboxCtrl', function () {
-        
-        beforeEach(inject(function ($rootScope, $controller) {
-            scope = $rootScope.$new();
-            controller = $controller('GenericListCtrl', {
-                '$scope': scope
-            });
-        }));
+  describe('unSelectCtrl', function () {
 
-        it('Correct change of checkboxState', function () {
-            scope.data.markedShows = new ShowsArray([
-                {id: 1234, title: 'element0', checkboxState: true, channel: {logo: null}, description: ""}]);
-            scope.toggleCheckbox(scope.data.markedShows[0]);
-            scope.$digest();
-            expect(scope.data.markedShows[0].checkboxState).toBe(false);
+    it('Correct deleting from selected list', function () {
+      $httpBackend.expectGET('/programs');
+      var controller = createController();
+      $httpBackend.flush();
+      var scope=$rootScope;
 
-            scope.toggleCheckbox(scope.data.markedShows[0]);
-            scope.$digest();
-            expect(scope.data.markedShows[0].checkboxState).toBe(true);
+      var foo = scope.data.showsPropertiesOfInterest[1];
 
-            
-        });
+      // We move all the elements of showsPropertiesOfInterest to markedShows
+      scope.select(scope.data.showsPropertiesOfInterest[0]);
+      scope.$digest();
+      scope.select(scope.data.showsPropertiesOfInterest[0]);
+      scope.$digest();
+      scope.select(scope.data.showsPropertiesOfInterest[0]);
+      scope.$digest();
 
-        
+      scope.unSelect(scope.data.markedShows[0]);
+      scope.$digest();
+      expect(scope.data.markedShows.length).toBe(2);
+      expect(scope.data.markedShows[0]).toBe(foo);
 
+      scope.unSelect(scope.data.markedShows[0]);
+      scope.$digest();
+      expect(scope.data.markedShows.length).toBe(1);
+
+      scope.unSelect(scope.data.markedShows[0]);
+      scope.$digest();
+      expect(scope.data.markedShows.length).toBe(0);   
     });
 
+    it('Correct move from selected list to unselected list', function () {
+      $httpBackend.expectGET('/programs');
+      var controller = createController();
+      $httpBackend.flush();
+      var scope=$rootScope;
+      scope.select(scope.data.showsPropertiesOfInterest[0]);
+      scope.$digest();
+      scope.select(scope.data.showsPropertiesOfInterest[0]);
+      scope.$digest();
+      scope.select(scope.data.showsPropertiesOfInterest[0]);
+      scope.$digest();
 
+      var temp=scope.data.markedShows[2];
+
+      scope.unSelect(scope.data.markedShows[2]);
+
+      scope.$digest();
+      expect(scope.data.markedShows.length).toBe(2);
+
+      expect(scope.data.showsPropertiesOfInterest.length).toBe(1);
+
+      expect(scope.data.showsPropertiesOfInterest[0]).toBe(temp);
+    });
+
+    it('Correct ordered insert in unselected list', function () {
+      $httpBackend.expectGET('/programs');
+      var controller = createController();
+      $httpBackend.flush();
+      var scope=$rootScope;
+
+      var e1 = scope.data.showsPropertiesOfInterest[0];
+      var e2 = scope.data.showsPropertiesOfInterest[1];
+      var e3 = scope.data.showsPropertiesOfInterest[2];
+      scope.select(e1);
+      scope.select(e2);
+      scope.select(e3);
+      scope.$digest();
+
+      scope.unSelect(e3);
+      scope.$digest();
+      expect(scope.data.showsPropertiesOfInterest[0].title).toBe(e3.title);
+
+      scope.unSelect(e1);
+      scope.$digest();
+      expect(scope.data.showsPropertiesOfInterest[0].title).toBe(e1.title);
+    });
+  });
+
+  describe('toggleCheckboxCtrl', function () {
+
+    it('Correct change of checkboxState', function () {
+      $httpBackend.expectGET('/programs');
+      var controller = createController();
+      $httpBackend.flush();
+      var scope=$rootScope;
+
+      scope.toggleCheckbox(scope.data.showsPropertiesOfInterest[0]);
+      scope.$digest();
+      expect(scope.data.showsPropertiesOfInterest[0].checkboxState).toBe(true);
+
+      scope.toggleCheckbox(scope.data.showsPropertiesOfInterest[0]);
+      scope.$digest();
+      expect(scope.data.showsPropertiesOfInterest[0].checkboxState).toBe(false);   
+    });
+  });
 });
-
-//TODO : connection with real server - retriving the real JSON content
 
 
 //Temporary JSON data
@@ -217,68 +206,67 @@ function getData(){
     } ],
     "synopsis" : [ ]
   }
-}, {
-  "id" : 15279,
-  "title" : "SessÃ£o PlenÃ¡ria",
-  "description" : "A atividade legislativa do Senado Federal se dÃ¡ principalmente por meio de reuniÃµes das comissÃµes e sessÃµes plenÃ¡rias. Para saber mais: www.senado.gov.br/tv",
-  "start" : 1380682800000,
-  "end" : 1380709800000,
-  "duration" : 450,
-  "channel" : {
-    "name" : "TV Senado",
-    "abbrev" : "SEN",
-    "logo" : "http://img.tvstar.com.br/channel/SEN.jpg",
-    "fibraLineup" : "183"
-  },
-  "content" : {
-    "idShow" : 1254,
+  }, {
+    "id" : 15279,
     "title" : "SessÃ£o PlenÃ¡ria",
     "description" : "A atividade legislativa do Senado Federal se dÃ¡ principalmente por meio de reuniÃµes das comissÃµes e sessÃµes plenÃ¡rias. Para saber mais: www.senado.gov.br/tv",
-    "slug" : "sessao-plenaria",
-    "type" : "Jornalismo",
-    "genre" : "PolÃ¬tico",
-    "rating" : "Programa livre para todas as idades",
-    "images" : [ {
-      "id" : 1030,
-      "url" : "http://5",
-      "width" : 0,
-      "height" : 0
-    } ],
-    "synopsis" : [ {
-      "id" : 2510,
-      "text" : "",
-      "created" : 1380664800000
-    } ]
-  }
-}, {
-  "id" : 15381,
-  "title" : "LegiÃ£o Estrangeira",
-  "description" : "A participaÃ§Ã£o do PaÃ¬s em temas mundiais e os fatos do cenÃ¡rio nacional sÃ£o comentados na atraÃ§Ã£o comandada pela jornalista MÃ´nica Teixeira num debate com correspondentes internacionais.",
-  "start" : 1380690000000,
-  "end" : 1380702600000,
-  "duration" : 210,
-  "channel" : {
-    "name" : "Cultura",
-    "abbrev" : "CUL",
-    "logo" : "http://img.tvstar.com.br/channel/CUL.jpg",
-    "fibraLineup" : "2"
-  },
-  "content" : {
-    "idShow" : 2327,
+    "start" : 1380682800000,
+    "end" : 1380709800000,
+    "duration" : 450,
+    "channel" : {
+      "name" : "TV Senado",
+      "abbrev" : "SEN",
+      "logo" : "http://img.tvstar.com.br/channel/SEN.jpg",
+      "fibraLineup" : "183"
+    },
+    "content" : {
+      "idShow" : 1254,
+      "title" : "SessÃ£o PlenÃ¡ria",
+      "description" : "A atividade legislativa do Senado Federal se dÃ¡ principalmente por meio de reuniÃµes das comissÃµes e sessÃµes plenÃ¡rias. Para saber mais: www.senado.gov.br/tv",
+      "slug" : "sessao-plenaria",
+      "type" : "Jornalismo",
+      "genre" : "PolÃ¬tico",
+      "rating" : "Programa livre para todas as idades",
+      "images" : [ {
+        "id" : 1030,
+        "url" : "http://5",
+        "width" : 0,
+        "height" : 0
+      } ],
+      "synopsis" : [ {
+        "id" : 2510,
+        "text" : "",
+        "created" : 1380664800000
+      } ]
+    }
+  }, {
+    "id" : 15381,
     "title" : "LegiÃ£o Estrangeira",
     "description" : "A participaÃ§Ã£o do PaÃ¬s em temas mundiais e os fatos do cenÃ¡rio nacional sÃ£o comentados na atraÃ§Ã£o comandada pela jornalista MÃ´nica Teixeira num debate com correspondentes internacionais.",
-    "slug" : "legiao-estrangeira",
-    "type" : "Jornalismo",
-    "genre" : "Informativo",
-    "rating" : "Programa livre para todas as idades",
-    "images" : [ {
-      "id" : 5205,
-      "url" : "http://5",
-      "width" : 0,
-      "height" : 0
-    } ],
-    "synopsis" : [ ]
-  }
-} ]
-
+    "start" : 1380690000000,
+    "end" : 1380702600000,
+    "duration" : 210,
+    "channel" : {
+      "name" : "Cultura",
+      "abbrev" : "CUL",
+      "logo" : "http://img.tvstar.com.br/channel/CUL.jpg",
+      "fibraLineup" : "2"
+    },
+    "content" : {
+      "idShow" : 2327,
+      "title" : "LegiÃ£o Estrangeira",
+      "description" : "A participaÃ§Ã£o do PaÃ¬s em temas mundiais e os fatos do cenÃ¡rio nacional sÃ£o comentados na atraÃ§Ã£o comandada pela jornalista MÃ´nica Teixeira num debate com correspondentes internacionais.",
+      "slug" : "legiao-estrangeira",
+      "type" : "Jornalismo",
+      "genre" : "Informativo",
+      "rating" : "Programa livre para todas as idades",
+      "images" : [ {
+        "id" : 5205,
+        "url" : "http://5",
+        "width" : 0,
+        "height" : 0
+      } ],
+      "synopsis" : [ ]
+    }
+  } ]
 }
