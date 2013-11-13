50600a59712fb239d97f6886e9ada220a766b32b
CarlosPerson <carlos.person.haro@gmail.com>
2013-11-13 08:45:14 +0100

    Minor changes

--- tests/static/js/genericListCtrl.test.js
+++ tests/static/js/genericListCtrl.test.js
@@ -1,37 +1,38 @@
 describe('GenericListCtrl', function () {
 
-    var scope, controller;
-    beforeEach(function () {
-        module('liveScannerApp');
-    });
+  var scope, controller;
+  beforeEach(function () {
+    module('liveScannerApp');
+  });
 
-    var $httpBackend, $rootScope, createController,$controller;
+  var $httpBackend, $rootScope, $controller, createController;
 
-        beforeEach(inject(function($injector) {
-            $httpBackend = $injector.get('$httpBackend');
-            $httpBackend.whenGET("/programs").respond(getData());
-            $rootScope = $injector.get('$rootScope');   
-            $controller = $injector.get('$controller');
+  beforeEach(inject(function($injector) {
+    $rootScope = $injector.get('$rootScope');   
+    $controller = $injector.get('$controller');
+    $httpBackend = $injector.get('$httpBackend');
+    createController = function() {
+      return $controller('GenericListCtrl', {
+        '$scope' : $rootScope
+      });
+    };
+  }));
 
-            createController = function() {
-            return $controller('GenericListCtrl', {
-                '$scope' : $rootScope 
-            });
-            };
-        }));
+  describe('When attempting fake HTTP connections for getting the source API content', function() {
 
+    beforeEach(inject(function($injector) {
+      $httpBackend.whenGET("/programs").respond(getData());
+    }));
 
-   describe('When attempting fake HTTP connections for getting the source API content', function() {
-      it("should perform GET http call to /programs", inject(function ($httpBackend) {
-        $httpBackend.expectGET('/programs');
-        var controller = createController();
-        $httpBackend.flush();
-        expect($rootScope.data.showsPropertiesOfInterest[0].title).not.toEqual("");
-      }));
+    it("should perform GET http call to /programs and retrieve a correct result", function () {
+      $httpBackend.expectGET('/programs');
+      var controller = createController();
+      $httpBackend.flush();
+      expect($rootScope.data.showsPropertiesOfInterest[0].title).toEqual("Encontro com Deus na Madrugada");
     });
 
     describe('SelectCtrl', function() {
-        
+
       it('Selecting a show from showsPropertiesOfInterest list, it should disapear from this list', function () {
         httpBackend.expectGET('/programs');
         var controller = createController();
@@ -52,7 +53,7 @@ describe('GenericListCtrl', function () {
         var controller = createController();
         $httpBackend.flush();
         var scope=$rootScope;
-        
+
         var temp=scope.data.showsPropertiesOfInterest[0];
         scope.select(scope.data.showsPropertiesOfInterest[0]);
 
@@ -75,100 +76,101 @@ describe('GenericListCtrl', function () {
       });        
     });
 
-  describe('unSelectCtrl', function () {
+    describe('unSelectCtrl', function () {
 
-    it('Unselecting a show from markedShows list, it should disapear from this list', function () {
-      $httpBackend.expectGET('/programs');
-      var controller = createController();
-      $httpBackend.flush();
-      var scope=$rootScope;
+      it('Unselecting a show from markedShows list, it should disapear from this list', function () {
+        $httpBackend.expectGET('/programs');
+        var controller = createController();
+        $httpBackend.flush();
+        var scope=$rootScope;
 
-      var foo = scope.data.showsPropertiesOfInterest[1];
+        var foo = scope.data.showsPropertiesOfInterest[1];
 
-      scope.select(scope.data.showsPropertiesOfInterest[0]);
-      scope.$digest();
-      scope.select(scope.data.showsPropertiesOfInterest[0]);
-      scope.$digest();
-      scope.select(scope.data.showsPropertiesOfInterest[0]);
-      scope.$digest();
+        scope.select(scope.data.showsPropertiesOfInterest[0]);
+        scope.$digest();
+        scope.select(scope.data.showsPropertiesOfInterest[0]);
+        scope.$digest();
+        scope.select(scope.data.showsPropertiesOfInterest[0]);
+        scope.$digest();
 
-      scope.unSelect(scope.data.markedShows[0]);
-      scope.$digest();
-      expect(scope.data.markedShows.length).toBe(2);
-      expect(scope.data.markedShows[0]).toBe(foo);
+        scope.unSelect(scope.data.markedShows[0]);
+        scope.$digest();
+        expect(scope.data.markedShows.length).toBe(2);
+        expect(scope.data.markedShows[0]).toBe(foo);
 
-      scope.unSelect(scope.data.markedShows[0]);
-      scope.$digest();
-      expect(scope.data.markedShows.length).toBe(1);
+        scope.unSelect(scope.data.markedShows[0]);
+        scope.$digest();
+        expect(scope.data.markedShows.length).toBe(1);
 
-      scope.unSelect(scope.data.markedShows[0]);
-      scope.$digest();
-      expect(scope.data.markedShows.length).toBe(0);   
+        scope.unSelect(scope.data.markedShows[0]);
+        scope.$digest();
+        expect(scope.data.markedShows.length).toBe(0);   
+      });
+
+      it('Unselecting a show from markedShows list, it should increment by 1 showsPropertiesOfInterest length', function () {
+        $httpBackend.expectGET('/programs');
+        var controller = createController();
+        $httpBackend.flush();
+        var scope=$rootScope;
+        scope.select(scope.data.showsPropertiesOfInterest[0]);
+        scope.$digest();
+        scope.select(scope.data.showsPropertiesOfInterest[0]);
+        scope.$digest();
+        scope.select(scope.data.showsPropertiesOfInterest[0]);
+        scope.$digest();
+
+        var temp=scope.data.markedShows[2];
+
+        scope.unSelect(scope.data.markedShows[2]);
+
+        scope.$digest();
+        expect(scope.data.markedShows.length).toBe(2);
+
+        expect(scope.data.showsPropertiesOfInterest.length).toBe(1);
+
+        expect(scope.data.showsPropertiesOfInterest[0]).toBe(temp);
+      });
+
+      it('Unselecting a show from markedShows list, it should be in showsPropertiesOfInterest list ordered by title', function () {
+        $httpBackend.expectGET('/programs');
+        var controller = createController();
+        $httpBackend.flush();
+        var scope=$rootScope;
+
+        var e1 = scope.data.showsPropertiesOfInterest[0];
+        var e2 = scope.data.showsPropertiesOfInterest[1];
+        var e3 = scope.data.showsPropertiesOfInterest[2];
+        scope.select(e1);
+        scope.select(e2);
+        scope.select(e3);
+        scope.$digest();
+
+        scope.unSelect(e3);
+        scope.$digest();
+        expect(scope.data.showsPropertiesOfInterest[0].title).toBe(e3.title);
+
+        scope.unSelect(e1);
+        scope.$digest();
+        expect(scope.data.showsPropertiesOfInterest[0].title).toBe(e1.title);
+      });
     });
 
-    it('Unselecting a show from markedShows list, it should increment by 1 showsPropertiesOfInterest length', function () {
-      $httpBackend.expectGET('/programs');
-      var controller = createController();
-      $httpBackend.flush();
-      var scope=$rootScope;
-      scope.select(scope.data.showsPropertiesOfInterest[0]);
-      scope.$digest();
-      scope.select(scope.data.showsPropertiesOfInterest[0]);
-      scope.$digest();
-      scope.select(scope.data.showsPropertiesOfInterest[0]);
-      scope.$digest();
+    describe('toggleCheckboxCtrl', function () {
 
-      var temp=scope.data.markedShows[2];
+      it('Correct change of property checkbox in a show', function () {
+        $httpBackend.expectGET('/programs');
+        var controller = createController();
+        $httpBackend.flush();
+        var scope=$rootScope;
 
-      scope.unSelect(scope.data.markedShows[2]);
+        scope.toggleCheckbox(scope.data.showsPropertiesOfInterest[0]);
+        scope.$digest();
+        expect(scope.data.showsPropertiesOfInterest[0].checkboxState).toBe(true);
 
-      scope.$digest();
-      expect(scope.data.markedShows.length).toBe(2);
-
-      expect(scope.data.showsPropertiesOfInterest.length).toBe(1);
-
-      expect(scope.data.showsPropertiesOfInterest[0]).toBe(temp);
-    });
-
-    it('Unselecting a show from markedShows list, it should be in showsPropertiesOfInterest list ordered by title', function () {
-      $httpBackend.expectGET('/programs');
-      var controller = createController();
-      $httpBackend.flush();
-      var scope=$rootScope;
-
-      var e1 = scope.data.showsPropertiesOfInterest[0];
-      var e2 = scope.data.showsPropertiesOfInterest[1];
-      var e3 = scope.data.showsPropertiesOfInterest[2];
-      scope.select(e1);
-      scope.select(e2);
-      scope.select(e3);
-      scope.$digest();
-
-      scope.unSelect(e3);
-      scope.$digest();
-      expect(scope.data.showsPropertiesOfInterest[0].title).toBe(e3.title);
-
-      scope.unSelect(e1);
-      scope.$digest();
-      expect(scope.data.showsPropertiesOfInterest[0].title).toBe(e1.title);
-    });
-  });
-
-  describe('toggleCheckboxCtrl', function () {
-
-    it('Correct change of property checkbox in a show', function () {
-      $httpBackend.expectGET('/programs');
-      var controller = createController();
-      $httpBackend.flush();
-      var scope=$rootScope;
-
-      scope.toggleCheckbox(scope.data.showsPropertiesOfInterest[0]);
-      scope.$digest();
-      expect(scope.data.showsPropertiesOfInterest[0].checkboxState).toBe(true);
-
-      scope.toggleCheckbox(scope.data.showsPropertiesOfInterest[0]);
-      scope.$digest();
-      expect(scope.data.showsPropertiesOfInterest[0].checkboxState).toBe(false);   
+        scope.toggleCheckbox(scope.data.showsPropertiesOfInterest[0]);
+        scope.$digest();
+        expect(scope.data.showsPropertiesOfInterest[0].checkboxState).toBe(false);   
+      });
     });
   });
 });
@@ -177,34 +179,34 @@ describe('GenericListCtrl', function () {
 //Temporary JSON data
 function getData(){
   return [ {
-  "id" : 15068,
-  "title" : "Encontro com Deus na Madrugada",
-  "description" : "Nas madrugadas a programaÃ§Ã£o da Rede Gospel traz os melhores programas da semana. O que vocÃª nÃ£o pode ver durante o dia, vocÃª confere atÃ© o amanhecer no Encontro com Deus na Madrugada.",
-  "start" : 1380684600000,
-  "end" : 1380704400000,
-  "duration" : 330,
-  "channel" : {
-    "name" : "Rede Gospel",
-    "abbrev" : "GOS",
-    "logo" : "http://img.tvstar.com.br/channel/GOS.jpg",
-    "fibraLineup" : "21"
-  },
-  "content" : {
-    "idShow" : 3987,
+    "id" : 15068,
     "title" : "Encontro com Deus na Madrugada",
     "description" : "Nas madrugadas a programaÃ§Ã£o da Rede Gospel traz os melhores programas da semana. O que vocÃª nÃ£o pode ver durante o dia, vocÃª confere atÃ© o amanhecer no Encontro com Deus na Madrugada.",
-    "slug" : "encontro-com-deus-na-madrugada",
-    "type" : "Variedades",
-    "genre" : "ReligiÃ£o",
-    "rating" : "Programa livre para todas as idades",
-    "images" : [ {
-      "id" : 1390,
-      "url" : "http://2",
-      "width" : 0,
-      "height" : 0
-    } ],
-    "synopsis" : [ ]
-  }
+    "start" : 1380684600000,
+    "end" : 1380704400000,
+    "duration" : 330,
+    "channel" : {
+      "name" : "Rede Gospel",
+      "abbrev" : "GOS",
+      "logo" : "http://img.tvstar.com.br/channel/GOS.jpg",
+      "fibraLineup" : "21"
+    },
+    "content" : {
+      "idShow" : 3987,
+      "title" : "Encontro com Deus na Madrugada",
+      "description" : "Nas madrugadas a programaÃ§Ã£o da Rede Gospel traz os melhores programas da semana. O que vocÃª nÃ£o pode ver durante o dia, vocÃª confere atÃ© o amanhecer no Encontro com Deus na Madrugada.",
+      "slug" : "encontro-com-deus-na-madrugada",
+      "type" : "Variedades",
+      "genre" : "ReligiÃ£o",
+      "rating" : "Programa livre para todas as idades",
+      "images" : [ {
+        "id" : 1390,
+        "url" : "http://2",
+        "width" : 0,
+        "height" : 0
+      } ],
+      "synopsis" : [ ]
+    }
   }, {
     "id" : 15279,
     "title" : "SessÃ£o PlenÃ¡ria",
